ccbfaaf3709b96f477546efbac85a3f420c004cf
Henry Eze <henryeze019@gmail.com>
2021-07-01 17:13:13 -0700

    responsive navigation completed

--- src/components/ResponsiveNavigation.vue
+++ src/components/ResponsiveNavigation.vue
@@ -6,9 +6,16 @@
       /></custom-button>
     </header>
     <div class="input-container">
-      <input type="text" />
-      <img src="" alt="" />
+      <input type="text" placeholder="Type your search" />
+      <img src="../assets/images/search.svg" alt="" />
     </div>
+    <nav>
+      <ul>
+        <li v-for="(route, index) in routeList" :key="index">
+          {{ route.name }}
+        </li>
+      </ul>
+    </nav>
   </div>
 </template>
 
@@ -17,6 +24,24 @@ import CustomButton from "./CustomButton.vue";
 export default {
   components: { CustomButton },
   name: "ResponsiveNavigation",
+  data: () => {
+    return {
+      routeList: [
+        {
+          name: "Vikings",
+        },
+        {
+          name: "Ancient Egypt",
+        },
+        {
+          name: "Celtics",
+        },
+        {
+          name: "SALE",
+        },
+      ],
+    };
+  },
   methods: {
     closeModal() {
       this.$store.commit("setModal", {});
@@ -35,5 +60,36 @@ export default {
 header {
   display: flex;
   padding: 20px;
+}
+.input-container {
+  display: flex;
+  align-items: center;
+  border-bottom: 1px solid $border-ash;
+  justify-content: center;
+  margin: 20px;
+  input {
+    border: transparent;
+    outline: none;
+    width: 90%;
+    background: transparent;
+    color: $white;
+  }
+}
+ul {
+  padding: 20px;
+  margin: 0px;
+  list-style-type: none;
+}
+li {
+  color: $white;
+  text-align: left;
+  border-bottom: 1px solid $border-ash;
+  padding: 10px;
+  font-weight: bold;
+  font-family: "Mulish", sans-serif;
+  font-size: 1.2em;
+  &:last-child {
+    color: $red;
+  }
 }
 </style>
